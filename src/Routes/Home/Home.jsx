3324dfb08b0966edc4bd3ece2937a730d6d4f3db
abdullokkg <abdullokh.akromov@gmail.com>
2024-05-21 22:11:21 +0500

    first commit

---
+++ src/Routes/Home/Home.jsx
@@ -0,0 +1,69 @@
+
+function Home() {
+  return (
+    <>
+      <div className="home_nav">
+        <nav className="nav">
+          <ul className="ul">
+            <li className="item">Works</li>
+            <li className="item">Blog</li>
+            <li className="item">Contact</li>
+          </ul>
+        </nav>
+      </div>
+      <div className="home_hero">
+        <div className="hero">
+          <h1 className="items">Hi, I am Abdulloh,<br />Frontend Developer</h1>
+          <br />
+          <p className="items_p">I am 14 years old. <br />
+            I am study at Najot Talim educational center.
+            <br />
+            I am also attended Mars It School before.
+            <br />
+            I am currently in the last month of my salvation.
+            <br />
+            Tell:+998(93)546-48-42
+            <br />
+            Telegram:@abdullokg
+            </p>
+        </div>
+      </div>
+      <div className="home_header">
+        <div className="header">
+          <h1 className="h1">Skill</h1>
+        </div>
+        <br />
+        <br />
+        <div className="header_skill">
+          <p className="p">Html, Css, Scss, React,<br /><br /> Redux, Redux Toolkit, Type script,
+          <br /><br /> Python, Java script</p>
+        </div>
+      </div>
+      <div className="home_hero2">
+          <div className="works2">
+            <div className="works_txt">
+              <h1 className="h1">Works</h1>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="works">
+            <a href="https://final-exam-vkcm.vercel.app/">
+              <h1 className="h1">Ebay</h1>
+              <br />
+              <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, dicta deserunt! Sequi expedita quam tempore! Fuga eaque nesciunt id reiciendis, magnam aliquam, voluptatibus quibusdam, necessitatibus qui quae quis ratione? Saepe?</p>
+            </a>
+            <a href="https://lustrous-palmier-24c265.netlify.app/">
+              <h1 className="h1">Product</h1>
+              <br />
+              <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsum maiores quae eveniet libero consequatur, eius hic aut praesentium nihil? Quos, reprehenderit saepe consequatur id vel quasi tempora. Pariatur, nihil!</p>
+            </a>
+            </div>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
