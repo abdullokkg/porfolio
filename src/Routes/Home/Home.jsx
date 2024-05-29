2da0476c2cc07ac5a1fd81f5ebf63487699e800b
abdullokkg <abdullokh.akromov@gmail.com>
2024-05-29 18:12:27 +0500

    second commit

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -5,17 +5,18 @@ function Home() {
       <div className="home_nav">
         <nav className="nav">
           <ul className="ul">
-            <li className="item">Works</li>
-            <li className="item">Blog</li>
-            <li className="item">Contact</li>
+            <li className="item"><a href="#works">Works</a></li>
+            <li className="item"><a href="#skills">Skills</a></li> 
+            <li className="item"><a href="#about">About me</a></li> 
+            <li className="item"><a href="#contact">Contact</a></li>
           </ul>
         </nav>
       </div>
-      <div className="home_hero">
+      <div id="about" className="home_hero">
         <div className="hero">
           <h1 className="items">Hi, I am Abdulloh,<br />Frontend Developer</h1>
           <br />
-          <p className="items_p">I am 14 years old. <br />
+          <p id="contact" className="items_p">I am 14 years old. <br />
             I am study at Najot Talim educational center.
             <br />
             I am also attended Mars It School before.
@@ -28,18 +29,25 @@ function Home() {
             </p>
         </div>
       </div>
-      <div className="home_header">
+      <div id="skills" className="home_header">
         <div className="header">
-          <h1 className="h1">Skill</h1>
+          <h1 className="h1">Soft Skills</h1>
         </div>
         <br />
         <br />
         <div className="header_skill">
-          <p className="p">Html, Css, Scss, React,<br /><br /> Redux, Redux Toolkit, Type script,
-          <br /><br /> Python, Java script</p>
+          <p className="p">Planning & organizational <br />Focus & attention <br />Communication (and people  in general) <br />Troubleshootyour code <br />Manage unexpected urgent tasks & roadblocks <br />Learn new information & knowledge</p>
+        </div>
+        <div className="header">
+          <h1 className="h1">Hard Skills</h1>
+        </div>
+        <br />
+        <br />
+        <div className="header_skill">
+          <p className="p">Html and Css, Scss,<br />React, Typescript, Redux <br />Bootstrapp, Tailwind, Javascript, <br />Python, Next.js, Json, <br />Redux Toolkit</p>
         </div>
       </div>
-      <div className="home_hero2">
+      <div id="works" className="home_hero2">
           <div className="works2">
             <div className="works_txt">
               <h1 className="h1">Works</h1>
@@ -49,15 +57,15 @@ function Home() {
             <br />
             <br />
             <div className="works">
-            <a href="https://final-exam-vkcm.vercel.app/">
+            <a className="a" href="https://final-exam-vkcm.vercel.app/">
               <h1 className="h1">Ebay</h1>
               <br />
-              <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, dicta deserunt! Sequi expedita quam tempore! Fuga eaque nesciunt id reiciendis, magnam aliquam, voluptatibus quibusdam, necessitatibus qui quae quis ratione? Saepe?</p>
+              <img className="img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/EBay_logo.svg/1200px-EBay_logo.svg.png" alt="" />
             </a>
-            <a href="https://lustrous-palmier-24c265.netlify.app/">
+            <a className="a" href="https://lustrous-palmier-24c265.netlify.app/">
               <h1 className="h1">Product</h1>
               <br />
-              <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis ipsum maiores quae eveniet libero consequatur, eius hic aut praesentium nihil? Quos, reprehenderit saepe consequatur id vel quasi tempora. Pariatur, nihil!</p>
+              <img className="img" src="https://img.etimg.com/thumb/width-1200,height-1200,imgsize-63314,resizemode-75,msid-100966025/jobs/c-suite/the-product-managers-toolkit-essential-skills-and-resources-for-success.jpg" alt="" />
             </a>
             </div>
           </div>
